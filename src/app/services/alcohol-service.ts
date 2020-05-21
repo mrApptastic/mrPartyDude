eb32d4f7f24355bbd9224e2c7a6d740aa2536a83
mrApptastic <30831839+mrApptastic@users.noreply.github.com>
2020-05-21 12:43:38 +0200

    Added Alcoholservice

---
+++ src/app/services/alcohol-service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AlcoholService {
+
+  constructor() { }
+
+  calculatePromille(genstande: number, minutes: number, gender: boolean ): number {
+    // https://www.sundhed.dk/borger/patienthaandbogen/psyke/sygdomme/alkohol/alkoholpromille-beregning/
+
+    // https://www.sundhed.dk/borger/patienthaandbogen/psyke/sygdomme/alkohol/alkohol-fakta/
+    return 0;
+  }
+}
