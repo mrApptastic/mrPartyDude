452a5bb8cb0663a797bd7db4361070f39984ff30
mrApptastic <henrikbeske@gmail.com>
2021-12-27 14:00:31 +0100

    Added unit calculation formula.

--- ClientApp/src/app/services/alcohol.service.ts
+++ ClientApp/src/app/services/alcohol.service.ts
@@ -7,7 +7,13 @@ export class AlcoholService {
 
   constructor() { }
 
+  calculateUnitFromAlcoholPercentage(liquidCl: number, alcoholPercentage: number): number {
+    // https://vinsiderne.dk/artikler/alkohol-vol
+    return liquidCl * alcoholPercentage * 0.7873 / 120;
+  }
+
   calculatePerMille(units: number, minutes: number, gender: boolean ): number {
+
     // https://www.sundhed.dk/borger/patienthaandbogen/psyke/sygdomme/alkohol/alkoholpromille-beregning/
 
     // https://www.sundhed.dk/borger/patienthaandbogen/psyke/sygdomme/alkohol/alkohol-fakta/
